d023d33314ae81585b9f2279ff90da6ae072e754
Geert <geert.boer555@gmail.com>
2023-10-31 18:23:21 +0100

    Create arduino_cloth_puller_teensy.ino

---
+++ arduino_cloth_puller_teensy/arduino_cloth_puller_teensy.ino
@@ -0,0 +1,103 @@
+#include <SimpleFOC.h>
+
+// magnetic sensor instance - SPI
+MagneticSensorSPI sensor = MagneticSensorSPI(AS5147_SPI, 10);
+// magnetic sensor instance - MagneticSensorI2C
+//MagneticSensorI2C sensor = MagneticSensorI2C(AS5600_I2C);
+// MagneticSensorAnalog sensor = MagneticSensorAnalog(A1, 14, 1020);
+
+// BLDC motor & driver instance
+BLDCMotor motor = BLDCMotor(7);
+BLDCDriver3PWM driver = BLDCDriver3PWM(2, 4, 3, 1);
+
+
+void setup() {
+  // initialise magnetic sensor hardware
+  sensor.init();
+  // link the motor to the sensor
+  motor.linkSensor(&sensor);
+
+  // driver config
+  // power supply voltage [V]
+  driver.voltage_power_supply = 12;
+  driver.init();
+  // link the motor and the driver
+  motor.linkDriver(&driver);
+
+  // choose FOC modulation (optional)
+  motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
+
+  // set motion control loop to be used
+  motor.controller = MotionControlType::angle;
+
+  // contoller configuration
+  // default parameters in defaults.h
+
+  // velocity PI controller parameters
+  motor.PID_velocity.P = 0.2f;
+  motor.PID_velocity.I = 20;
+  motor.PID_velocity.D = 0;
+  // maximal voltage to be set to the motor
+  motor.voltage_limit = 12;
+
+  // velocity low pass filtering time constant
+  // the lower the less filtered
+  motor.LPF_velocity.Tf = 0.01f;
+
+  // angle P controller
+  motor.P_angle.P = 20;
+  // maximal velocity of the position control
+  motor.velocity_limit = 20;
+
+  // use monitoring with serial
+  Serial.begin(115200);
+
+
+  // initialize motor
+  motor.init();
+  // align sensor and start FOC
+  motor.initFOC();
+
+}
+
+bool dir = true;
+
+unsigned long oldmillis = 0;
+float wait = 800;
+int ctr = 0;
+void loop() {
+  // main FOC algorithm function
+  // the faster you run this function the better
+  // Arduino UNO loop  ~1kHz
+  // Bluepill loop ~10kHz
+  motor.loopFOC();
+
+  // Motion control function
+  // velocity, position or voltage (defined in motor.controller)
+  // this function can be run at much lower frequency than loopFOC() function
+  // You can also use motor.move() and set the motor.target in the code
+
+  sensor.update();
+
+  if (dir) {
+    motor.move(-6.0);
+  }
+  else {
+    motor.move(3.0);
+  }
+
+  if ((millis() - oldmillis) > wait) {
+    Serial.println(sensor.getAngle());
+    dir = !dir;
+    oldmillis = millis();
+    ctr++;
+    //    wait = wait / 1.025;
+    wait = random(50, 1000);
+  }
+
+  //  if (ctr == 3) {
+  //    delay(10);
+  //    ctr = 0;
+  //  }
+
+}
